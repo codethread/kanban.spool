b6cffe904e8bc47182fcf2a13a75f53b6048cc10
codethread <adamhalldesigns@gmail.com>
2026-08-13 20:38:32 +0100

    chore: configure portable spool tooling

--- .millstrand/init.clj
+++ .millstrand/init.clj
@@ -11,10 +11,29 @@
 ;; Batteries is approved as a shipped source-root spool; the :spools guard
 ;; keeps source loading behind that visible spools.edn approval.
 (runtime/module! runtime :millstrand/spools-batteries
-  {:ns 'millstrand.spools.batteries
-   :spools ['millstrand.spools/batteries]})
+                 {:ns 'millstrand.spools.batteries
+                  :spools ['millstrand.spools/batteries]})
+
+(runtime/module! runtime :millhouse/spools-workflow
+                 {:ns 'millhouse.spools.workflow
+                  :spools ['millhouse.spools/workflow]
+                  :required? true})
+
+(runtime/module! runtime :millhouse/spools-workflow-cli
+                 {:ns 'millhouse.spools.workflow.cli
+                  :spools ['millhouse.spools/workflow]
+                  :after [:millhouse/spools-workflow]
+                  :required? true})
+
+(runtime/module! runtime :millhouse/spools-millstrand-workflows
+                 {:ns 'millhouse.spools.millstrand-workflows
+                  :spools ['millhouse.spools/millstrand-workflows
+                           'millhouse.spools/workflow]
+                  :after [:millhouse/spools-workflow
+                          :millhouse/spools-workflow-cli]
+                  :required? true})
 
 (runtime/module! runtime :kanban
-  {:ns 'ct.spools.kanban
-   :spools ['codethread/kanban]
-   :required? true})
+                 {:ns 'ct.spools.kanban
+                  :spools ['codethread/kanban]
+                  :required? true})
